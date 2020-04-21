d9473ecccc2c3df46da33a088dd42f6d68dfe76a
nqthai309 <nqthai199@gmail.com>
2020-04-21 18:49:14 +0700

    done task nhan vien .

--- TTNhom/AddNhanVienForm.Designer.cs
+++ TTNhom/AddNhanVienForm.Designer.cs
@@ -28,28 +28,184 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtDiaChi = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButtonNu = new System.Windows.Forms.RadioButton();
+            this.radioButtonNam = new System.Windows.Forms.RadioButton();
+            this.txtTen = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.CustomFormat = "yyyy/MM/dd";
+            this.dateTimePicker2.Location = new System.Drawing.Point(472, 291);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(152, 20);
+            this.dateTimePicker2.TabIndex = 121;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "yyyy/MM/dd";
+            this.dateTimePicker1.Location = new System.Drawing.Point(119, 291);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(167, 20);
+            this.dateTimePicker1.TabIndex = 122;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(454, 219);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(167, 20);
+            this.txtPhone.TabIndex = 120;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(389, 222);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 13);
+            this.label7.TabIndex = 117;
+            this.label7.Text = "SĐT";
+            // 
+            // txtDiaChi
+            // 
+            this.txtDiaChi.Location = new System.Drawing.Point(454, 158);
+            this.txtDiaChi.Name = "txtDiaChi";
+            this.txtDiaChi.Size = new System.Drawing.Size(167, 20);
+            this.txtDiaChi.TabIndex = 119;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(389, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 118;
+            this.label2.Text = "Địa Chỉ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(389, 296);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 115;
+            this.label3.Text = "Ngày Vào Làm";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(271, 272);
+            this.label1.Location = new System.Drawing.Point(51, 296);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(151, 22);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Thêm mới nhân viên";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 116;
+            this.label1.Text = "Ngày Sinh";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.radioButtonNu);
+            this.panel1.Controls.Add(this.radioButtonNam);
+            this.panel1.Location = new System.Drawing.Point(119, 217);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(167, 25);
+            this.panel1.TabIndex = 114;
+            // 
+            // radioButtonNu
+            // 
+            this.radioButtonNu.AutoSize = true;
+            this.radioButtonNu.Location = new System.Drawing.Point(114, 3);
+            this.radioButtonNu.Name = "radioButtonNu";
+            this.radioButtonNu.Size = new System.Drawing.Size(39, 17);
+            this.radioButtonNu.TabIndex = 15;
+            this.radioButtonNu.TabStop = true;
+            this.radioButtonNu.Text = "Nữ";
+            this.radioButtonNu.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonNam
+            // 
+            this.radioButtonNam.AutoSize = true;
+            this.radioButtonNam.Location = new System.Drawing.Point(0, 3);
+            this.radioButtonNam.Name = "radioButtonNam";
+            this.radioButtonNam.Size = new System.Drawing.Size(47, 17);
+            this.radioButtonNam.TabIndex = 14;
+            this.radioButtonNam.TabStop = true;
+            this.radioButtonNam.Text = "Nam";
+            this.radioButtonNam.UseVisualStyleBackColor = true;
+            // 
+            // txtTen
+            // 
+            this.txtTen.Location = new System.Drawing.Point(119, 158);
+            this.txtTen.Name = "txtTen";
+            this.txtTen.Size = new System.Drawing.Size(167, 20);
+            this.txtTen.TabIndex = 113;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(51, 225);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 112;
+            this.label4.Text = "Giới tính";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(51, 161);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(26, 13);
+            this.label13.TabIndex = 111;
+            this.label13.Text = "Tên";
+            // 
+            // btnThem
+            // 
+            this.btnThem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(186)))), ((int)(((byte)(151)))));
+            this.btnThem.FlatAppearance.BorderSize = 0;
+            this.btnThem.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnThem.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThem.ForeColor = System.Drawing.Color.White;
+            this.btnThem.Location = new System.Drawing.Point(279, 380);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(120, 47);
+            this.btnThem.TabIndex = 123;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = false;
+            this.btnThem.Click += new System.EventHandler(this.BtnThem_Click);
             // 
             // AddNhanVienForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(675, 603);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtDiaChi);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtTen);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label13);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "AddNhanVienForm";
             this.Text = "AddNhanVienForm";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -57,6 +213,20 @@
 
         #endregion
 
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtDiaChi;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioButtonNu;
+        private System.Windows.Forms.RadioButton radioButtonNam;
+        private System.Windows.Forms.TextBox txtTen;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Button btnThem;
     }
 }
--- TTNhom/AddNhanVienForm.cs
+++ TTNhom/AddNhanVienForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,60 @@ namespace TTNhom
 {
     public partial class AddNhanVienForm : Form
     {
+        private static SqlConnection conn = new SqlConnection(DBAccess.connString);
+        private static SqlDataAdapter adt = new SqlDataAdapter();
+        private static SqlCommand cmd = new SqlCommand();
+        DBAccess access = new DBAccess();
+        DataTable table;
+        string ten, diaChi, ngaySinh, gioiTinh, sdt, ngayVaoLam;
         public AddNhanVienForm()
         {
             InitializeComponent();
+        }
+
+        private void BtnThem_Click(object sender, EventArgs e)
+        {
+            ten = txtTen.Text.Trim();
+            ngaySinh = dateTimePicker1.Value.Date.ToString("yyyy-MM-dd HH:mm:ss");
+            diaChi = txtDiaChi.Text;
+            sdt = txtPhone.Text;
+            ngayVaoLam = dateTimePicker2.Value.Date.ToString("yyyy-MM-dd HH:mm:ss");
+
+            if (radioButtonNam.Checked == true)
+            {
+                gioiTinh = "1";
+            }
+            else
+            {
+                gioiTinh = "0";
+            }
+            try
+            {
+                if (ten.Equals("") || ngaySinh.Equals("") || diaChi.Equals("") || sdt.Equals("") || gioiTinh.Equals(""))
+                {
+                    MessageBox.Show("Thieu Thong tin");
+
+                }
+                else
+                {
+                    conn.Open();
+                    table = new DataTable();
+                    string queryInsert = "INSERT dbo.NhanVien( HoTenNV ,DiaChiNV ,NgaySinh ,Sex ,SDT ,NgayVaoLam) VALUES" +
+                        " ( N'"+ten+"' , N'"+diaChi+"' , '"+ngaySinh+"' , '"+gioiTinh+"' , '"+sdt+"' , '"+ngayVaoLam+"' )";
+                    cmd = new SqlCommand(queryInsert, conn);
+                    cmd.CommandType = CommandType.Text;
+                    int i = cmd.ExecuteNonQuery();
+                    if (i != 0)
+                    {
+                        MessageBox.Show("Them Hoc Sinh Thanh Cong");
+                        conn.Close();
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Sai Định Dạng Ngày Tháng");
+            }
         }
     }
 }
