8f157292d30edcb5c1cf851c3f7ff2217d8c82d3
hoducduy99 <hoducduy99@gmail.com>
2020-04-22 08:14:52 +0700

    update

--- TTNhom/ListSachForm.Designer.cs
+++ TTNhom/ListSachForm.Designer.cs
@@ -28,35 +28,266 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tableMain = new System.Windows.Forms.DataGridView();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.comboBoxMaNXB = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxTacgia = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.textBoxTenSach = new System.Windows.Forms.TextBox();
+            this.numericUpDownSoLuong = new System.Windows.Forms.NumericUpDown();
+            this.textBoxNote = new System.Windows.Forms.TextBox();
+            this.comboBoxMaLoaiSach = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.tableMain)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSoLuong)).BeginInit();
             this.SuspendLayout();
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(542, 121);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(59, 23);
+            this.buttonSearch.TabIndex = 158;
+            this.buttonSearch.Text = "Tìm Kiếm";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSearch.Location = new System.Drawing.Point(387, 121);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(149, 23);
+            this.textBoxSearch.TabIndex = 157;
+            this.textBoxSearch.TextChanged += new System.EventHandler(this.textBoxSearch_TextChanged);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(53, 83);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(568, 17);
+            this.label12.TabIndex = 156;
+            this.label12.Text = "______________________________________________________________________";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(52, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(148, 24);
+            this.label3.TabIndex = 155;
+            this.label3.Text = "Danh sách Sách";
+            // 
+            // tableMain
+            // 
+            this.tableMain.BackgroundColor = System.Drawing.Color.White;
+            this.tableMain.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.tableMain.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tableMain.Location = new System.Drawing.Point(59, 164);
+            this.tableMain.MultiSelect = false;
+            this.tableMain.Name = "tableMain";
+            this.tableMain.ReadOnly = true;
+            this.tableMain.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.tableMain.RowHeadersVisible = false;
+            this.tableMain.RowHeadersWidth = 51;
+            this.tableMain.Size = new System.Drawing.Size(565, 167);
+            this.tableMain.TabIndex = 150;
+            this.tableMain.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.tableMain_CellClick);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(83)))), ((int)(((byte)(79)))));
+            this.btnXoa.FlatAppearance.BorderSize = 0;
+            this.btnXoa.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnXoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXoa.ForeColor = System.Drawing.Color.White;
+            this.btnXoa.Location = new System.Drawing.Point(412, 518);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(103, 41);
+            this.btnXoa.TabIndex = 154;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = false;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(186)))), ((int)(((byte)(151)))));
+            this.btnSua.FlatAppearance.BorderSize = 0;
+            this.btnSua.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSua.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSua.ForeColor = System.Drawing.Color.White;
+            this.btnSua.Location = new System.Drawing.Point(156, 518);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(103, 41);
+            this.btnSua.TabIndex = 153;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = false;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // comboBoxMaNXB
+            // 
+            this.comboBoxMaNXB.FormattingEnabled = true;
+            this.comboBoxMaNXB.Location = new System.Drawing.Point(436, 371);
+            this.comboBoxMaNXB.Name = "comboBoxMaNXB";
+            this.comboBoxMaNXB.Size = new System.Drawing.Size(185, 21);
+            this.comboBoxMaNXB.TabIndex = 170;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(341, 374);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 13);
+            this.label4.TabIndex = 169;
+            this.label4.Text = "Mã nhà xuất bản";
+            // 
+            // textBoxTacgia
+            // 
+            this.textBoxTacgia.Location = new System.Drawing.Point(436, 345);
+            this.textBoxTacgia.Name = "textBoxTacgia";
+            this.textBoxTacgia.Size = new System.Drawing.Size(185, 20);
+            this.textBoxTacgia.TabIndex = 168;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(270, 272);
+            this.label1.Location = new System.Drawing.Point(341, 347);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(119, 22);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Danh sách Sách";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 167;
+            this.label1.Text = "Tác giả";
+            // 
+            // textBoxTenSach
+            // 
+            this.textBoxTenSach.Location = new System.Drawing.Point(117, 345);
+            this.textBoxTenSach.Name = "textBoxTenSach";
+            this.textBoxTenSach.Size = new System.Drawing.Size(185, 20);
+            this.textBoxTenSach.TabIndex = 166;
+            // 
+            // numericUpDownSoLuong
+            // 
+            this.numericUpDownSoLuong.Location = new System.Drawing.Point(117, 398);
+            this.numericUpDownSoLuong.Name = "numericUpDownSoLuong";
+            this.numericUpDownSoLuong.Size = new System.Drawing.Size(74, 20);
+            this.numericUpDownSoLuong.TabIndex = 165;
+            // 
+            // textBoxNote
+            // 
+            this.textBoxNote.Location = new System.Drawing.Point(417, 397);
+            this.textBoxNote.Multiline = true;
+            this.textBoxNote.Name = "textBoxNote";
+            this.textBoxNote.Size = new System.Drawing.Size(207, 101);
+            this.textBoxNote.TabIndex = 164;
+            // 
+            // comboBoxMaLoaiSach
+            // 
+            this.comboBoxMaLoaiSach.FormattingEnabled = true;
+            this.comboBoxMaLoaiSach.Location = new System.Drawing.Point(117, 371);
+            this.comboBoxMaLoaiSach.Name = "comboBoxMaLoaiSach";
+            this.comboBoxMaLoaiSach.Size = new System.Drawing.Size(185, 21);
+            this.comboBoxMaLoaiSach.TabIndex = 163;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(343, 398);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(52, 13);
+            this.label8.TabIndex = 162;
+            this.label8.Text = "Nội Dung";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(21, 398);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(49, 13);
+            this.label7.TabIndex = 161;
+            this.label7.Text = "Số lượng";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(21, 374);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 13);
+            this.label6.TabIndex = 160;
+            this.label6.Text = "Mã loại sách";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 347);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 159;
+            this.label2.Text = "Tên sách";
             // 
             // ListSachForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(675, 603);
+            this.Controls.Add(this.comboBoxMaNXB);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxTacgia);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxTenSach);
+            this.Controls.Add(this.numericUpDownSoLuong);
+            this.Controls.Add(this.textBoxNote);
+            this.Controls.Add(this.comboBoxMaLoaiSach);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.textBoxSearch);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tableMain);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnSua);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "ListSachForm";
             this.Text = "ListSach";
+            ((System.ComponentModel.ISupportInitialize)(this.tableMain)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSoLuong)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView tableMain;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.ComboBox comboBoxMaNXB;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxTacgia;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxTenSach;
+        private System.Windows.Forms.NumericUpDown numericUpDownSoLuong;
+        private System.Windows.Forms.TextBox textBoxNote;
+        private System.Windows.Forms.ComboBox comboBoxMaLoaiSach;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label2;
     }
 }
--- TTNhom/ListSachForm.cs
+++ TTNhom/ListSachForm.cs
@@ -3,18 +3,134 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace TTNhom
 {
     public partial class ListSachForm : Form
     {
+        DBAccess access = new DBAccess();
+        DataTable table;
+
+        private static SqlConnection conn = new SqlConnection(DBAccess.connString);
+        private static SqlDataAdapter adt = new SqlDataAdapter();
+        private static SqlCommand cmd = new SqlCommand();
+
+        int maSach = -1;
+        string tenSach,tacGia,maLoaiSach,maNXB,noiDung;
+        int soLuong;
+        List<string> listSach = new List<string>();
+        List<string> listNXB = new List<string>();
         public ListSachForm()
         {
             InitializeComponent();
+            addComboBox(conn , cmd , listSach , "MaLoaiSach" , "LoaiSach" , comboBoxMaLoaiSach);
+            addComboBox(conn , cmd , listNXB , "MaNXB" , "NXB" , comboBoxMaNXB);
+            addData();
+        }
+        private void GetData(string query , DataGridView grid , DataTable table) {
+            access.createConn();
+            adt = new SqlDataAdapter(query , conn);
+            adt.Fill(table);
+            grid.DataSource = table;
+            conn.Close();
+        }
+        private void addComboBox(SqlConnection conn , SqlCommand cmd , List<string> list , string tenCot , string tenTable , ComboBox cb) {
+            conn.Open();
+            cmd = new SqlCommand("Select " + tenCot + " From " + tenTable , conn);
+            SqlDataReader dr = cmd.ExecuteReader();
+            while(dr.Read()) {
+                list.Add(dr.GetValue(0).ToString());
+            }
+            cb.DataSource = list;
+            conn.Close();
+        }
+        private void addData() {
+            table = new DataTable();
+            GetData("select * from DauSach" , tableMain , table);
+        }
+
+        private void tableMain_CellClick(object sender , DataGridViewCellEventArgs e) {
+            int index = e.RowIndex;
+            DataGridViewRow selectRow = tableMain.Rows[index];
+            maSach = int.Parse(selectRow.Cells[0].Value.ToString().Trim());
+            tenSach = selectRow.Cells[1].Value.ToString().Trim();
+            tacGia = selectRow.Cells[2].Value.ToString().Trim();
+            maLoaiSach = selectRow.Cells[3].Value.ToString().Trim();
+            maNXB = selectRow.Cells[4].Value.ToString().Trim();
+            soLuong = int.Parse(selectRow.Cells[5].Value.ToString().Trim());
+            noiDung = selectRow.Cells[6].Value.ToString().Trim();
+
+            textBoxTenSach.Text = tenSach;
+            textBoxTacgia.Text = tacGia;
+            textBoxNote.Text = noiDung;
+            numericUpDownSoLuong.Value = soLuong;
+            comboBoxMaLoaiSach.SelectedIndex = comboBoxMaLoaiSach.FindStringExact(maLoaiSach.ToString());
+            comboBoxMaNXB.SelectedIndex = comboBoxMaNXB.FindStringExact(maNXB.ToString());
+        }
+        private void btnXoa_Click(object sender , EventArgs e) {
+            if(maSach == -1) {
+                MessageBox.Show("Chưa chọn sách nào để xoá");
+            }
+            else {
+                table = new DataTable();
+                string query1 = "UPDATE dbo.NhuCauMuonTra SET MaSach = NULL WHERE MaSach = '" + maSach + "'";
+                string query2 = "DELETE dbo.DauSach WHERE MaSach = " + maSach;
+
+                GetData(query1 , tableMain , table);
+                GetData(query2 , tableMain , table);
+                GetData("select * from DauSach" , tableMain , table);
+                MessageBox.Show("Done");
+
+            }
+        }
+        private void btnSua_Click(object sender , EventArgs e) {
+            table = new DataTable();
+            tenSach = textBoxTenSach.Text.Trim();
+            tacGia = textBoxTacgia.Text.Trim();
+            maLoaiSach = comboBoxMaLoaiSach.Text.ToString();
+            noiDung = textBoxNote.Text.ToString();
+            maNXB = comboBoxMaNXB.Text.ToString();
+            soLuong = int.Parse(numericUpDownSoLuong.Value.ToString());
+
+            if(tenSach.Equals("")||tacGia.Equals("")) {
+                MessageBox.Show("Thieu Thong tin");
+            }
+            else {
+                string query = "UPDATE dbo.DauSach SET TenSach = N'" + tenSach + "' , TacGia = N' " + tacGia + "', MaLoaiSach = '" + maLoaiSach + "' , MaNXB = '" + maNXB + "' , SoLuong = " + soLuong + " , NoiDung = N'" + noiDung + "' WHERE MaSach = '" + maSach + "'";
+                GetData(query , tableMain , table);
+                GetData("select * from DauSach" , tableMain , table);
+                MessageBox.Show("Done");
+            }
+        }
+        private void textBoxSearch_TextChanged(object sender , EventArgs e) {
+            string keyword;
+            keyword = textBoxSearch.Text;
+            table = new DataTable();
+            GetData("select * from DauSach WHERE TenSach LIKE N'%" + keyword + "%' "
+                + "OR TacGia LIKE N'%" + keyword + "%' "
+                + "OR MaLoaiSach LIKE N'%" + keyword + "%'"
+                + "OR MaNXB LIKE N'%" + keyword + "%'"
+                + "OR SoLuong LIKE N'%" + keyword + "%'"
+                + "OR NoiDung LIKE N'%" + keyword + "%'"
+                , tableMain , table);
+        }
+        private void button3_Click(object sender , EventArgs e) {
+            string keyword;
+            keyword = textBoxSearch.Text;
+            table = new DataTable();
+            GetData("select * from DauSach WHERE TenSach LIKE N'%" + keyword + "%' " 
+                +"OR TacGia LIKE N'%" + keyword + "%' "
+                + "OR MaLoaiSach LIKE N'%" + keyword + "%'"
+                + "OR MaNXB LIKE N'%" + keyword + "%'"
+                + "OR SoLuong LIKE N'%" + keyword + "%'"
+                + "OR NoiDung LIKE N'%" + keyword + "%'"
+                , tableMain , table);
         }
     }
 }
